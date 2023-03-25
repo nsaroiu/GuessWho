5ff011b0b9994b8a8410e8a999a24809498c267b
Reddy-Boss-420 <prashanth.shyamala@mail.utoronto.ca>
2023-03-25 12:43:05 -0400

    smd

---
+++ GuessTree.py
@@ -0,0 +1,135 @@
+"""Copyright and Usage Information
+===============================
+Copyright (c) 2023 Prashanth Shyamala and Nicholas Saroiu
+All Rights Reserved.
+This program is the property of Prashanth Shyamala and Nicholas Saroiu.
+All forms of distribution of this code, whether as given or with any changes,
+are expressly prohibited.
+For more information on copyright for CSC111 materials, please consult our Course Syllabus.
+"""
+from __future__ import annotations
+from typing import Any, Optional
+import pandas as pd
+import numpy as np
+import re
+
+from python_ta.contracts import check_contracts
+
+df = pd.DataFrame({
+    # hairstyle
+    'hair_partition': [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0],
+    'curly_hair': [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+    'hat': [0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    'bald': [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+    'long_hair': [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    # hair color
+    'ginger_hair': [0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    'white_hair': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0],
+    'brown_hair': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+    'blond_hair': [0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    'black_hair': [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1],
+    # facial attributes
+    'big_mouth': [1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0],
+    'big_nose': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0],
+    'red_cheeks': [0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0],
+    'blue_eyes': [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    'sad_looking': [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    # facial hair
+    'facial_hair': [1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0],
+    'moustache': [1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+    'beard': [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+    # other
+    'glasses': [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
+    'earrings': [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    'female': [0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    # names
+    'name': ['alex', 'alfred', 'anita', 'anne', 'bernard', 'bill', 'charles', 'claire',
+             'david', 'eric', 'frans', 'george', 'herman', 'joe', 'maria', 'max', 'paul',
+             'peter', 'philip', 'richard', 'robert', 'sam', 'susan', 'tom']
+})
+
+column_description = {
+    "hair_partition": "Does your character have a visible hair partition?",
+    "curly_hair": "Does your character have curly hair?",
+    "hat": "Does your character wear a hat?",
+    "bald": "Is your character bald?",
+    "long_hair": "Does your character have long hair?",
+    "ginger_hair": "Does your character have ginger hair?",
+    "white_hair": "Does your character have white hair?",
+    "brown_hair": "Does your character have brown hair?",
+    "blond_hair": "Does your character have blond hair?",
+    "black_hair": "Does your character have black hair?",
+    "big_mouth": "Does your character have a big mouth?",
+    "big_nose": "Does your character have a big nose?",
+    "red_cheeks": "Does your character have red cheeks?",
+    "blue_eyes": "Does your character have blue eyes?",
+    "sad_looking": "Does your character look sad?",
+    "facial_hair": "Does your character have facial hair?",
+    "moustache": "Does your character have a moustache?",
+    "beard": "Does your character have a beard?",
+    "glasses": "Does your character wear glasses?",
+    "earrings": "Does your character wear earrings?",
+    "female": "Is your character female?"
+}
+
+
+@check_contracts
+class GuessTree:
+    """A binary tree class that stores the decision tree for a given dataset and algorithm.
+
+        Representation Invariants:
+          - (self._root is None) == (self._left is None)
+          - (self._root is None) == (self._right is None)
+          - If self._root is not None, then self._left and self._right are GuessTrees.
+
+        """
+    # Private Instance Attributes:
+    #   - _root:
+    #       The item stored at the root of this tree, or None if this tree is empty.
+    #   - _left:
+    #       The left subtree, or None if this tree is empty.
+    #   - _right:
+    #       The right subtree, or None if this tree is empty.
+    root: Optional[Any]
+    left: Optional[GuessTree]
+    right: Optional[GuessTree]
+    algorithm: str
+
+    def __init__(self, root: Optional[Any], algorithm: str = 'CART') -> None:
+        """Initialize a new GuessTree containing only the given root value.
+
+        If <root> is None, initialize an empty tree.
+        """
+        self.algorithm = algorithm
+
+        if root is None:
+            self.root = None
+            self.left = None
+            self.right = None
+        else:
+            self.root = root
+            self.left = GuessTree(None)
+            self.right = GuessTree(None)
+
+    def __str__(self) -> str:
+        """Return a string representation of this BST.
+
+        This string uses indentation to show depth.
+
+        """
+        return self.str_indented(0)
+
+    def str_indented(self, depth: int) -> str:
+        """Return an indented string representation of this BST.
+
+        The indentation level is specified by the <depth> parameter.
+
+        Preconditions:
+            - depth >= 0
+        """
+        if self.is_empty():
+            return ''
+        else:
+            return (depth * '  ' + f'{self._root}\n'
+                    + self.left.str_indented(depth + 1)
+                    + self.right.str_indented(depth + 1))
--- character.py
+++ character.py
@@ -1,7 +1,9 @@
 import numpy as np
+from __future__ import annotations
+from typing import Optional
 
 
-class Character():
+class Character:
     """A class representing a Guess Who character. Each character has a unique name and a unique set of identifiable
     features that players will use to identify their opponent's secret character.
 
@@ -16,3 +18,14 @@ class Character():
     def __init__(self, name: str, features: list[bool]) -> None:
         self.name = name
         self.features = np.array(features)
+
+
+if __name__ == '__main__':
+    import python_ta
+
+    python_ta.check_all(config={
+        'extra-imports': ['numpy', 'pandas'],
+        'allowed-io': [],
+        'max-line-length': 100,
+        'disable': ['E1136']
+    })
