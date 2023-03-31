7d151c750e52c2b4faef16f1e21a045e042dcc4c
Reddy-Boss-420 <prashanth.shyamala@mail.utoronto.ca>
2023-03-30 22:57:30 -0400

    yes

--- guesswho.py
+++ guesswho.py
@@ -14,7 +14,7 @@ import pandas as pd
 import numpy as np
 from character import Character
 import player
-import GuessTree as gt
+import GuessTree as GT
 
 from python_ta.contracts import check_contracts
 
