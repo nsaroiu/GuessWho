5d719a37a2ceda85b6890113c2f4d77241840735
Reddy-Boss-420 <prashanth.shyamala@mail.utoronto.ca>
2023-03-31 22:36:01 -0400

    Added a scraper file to scrape the character images into the images folder as jpegs

---
+++ Game.py
@@ -0,0 +1,67 @@
+"""Copyright and Usage Information
+===============================
+Copyright (c) 2023 Prashanth Shyamala and Nicholas Saroiu
+All Rights Reserved.
+This program is the property of Prashanth Shyamala and Nicholas Saroiu.
+All forms of distribution of this code, whether as given or with any changes,
+are expressly prohibited.
+For more information on copyright for CSC111 materials, please consult our Course Syllabus.
+"""
+from __future__ import annotations
+from typing import Any, Optional
+import pandas as pd
+import numpy as np
+import pygame
+
+from python_ta.contracts import check_contracts
+
+
+def main() -> None:
+    """Main function for the game.
+    """
+    import pygame
+
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("purple")
+
+        pygame.draw.circle(screen, "red", player_pos, 40)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            player_pos.y -= 300 * dt
+        if keys[pygame.K_s]:
+            player_pos.y += 300 * dt
+        if keys[pygame.K_a]:
+            player_pos.x -= 300 * dt
+        if keys[pygame.K_d]:
+            player_pos.x += 300 * dt
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        # limits FPS to 60
+        # dt is delta time in seconds since last frame, used for framerate-
+        # independent physics.
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
